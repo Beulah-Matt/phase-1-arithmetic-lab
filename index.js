b038ef454a67fc9e73d2a7b2ec1fd0353e1bfb20
Beulah Mathenge <beulahmathenge@gmail.com>
2022-08-15 23:26:03 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,2 +1,20 @@
+//Multiplication
 
-// Write your code here
+const num1 = 31
+const num2 = 2
+const multiply = num1*num2;
+
+multiply;
+
+//Random integers
+const random = Math.floor(Math.random() * 10);
+
+// Reminder of 4
+const num3 = 25;
+const num4 = 7;
+const mod = num3%num4;
+mod;
+
+//Max operator 
+const max =Math.max(5, 17, 2, 20);
+max;
